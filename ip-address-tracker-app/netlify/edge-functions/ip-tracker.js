a5141068afcfb9f0a4fa4a50b04bd91534baeb8a
carlosmndzg <cjr2344@gmail.com>
2024-07-28 19:55:21 +0200

    Solved IP bug

--- ip-address-tracker-app/netlify/edge-functions/ip-tracker.js
+++ ip-address-tracker-app/netlify/edge-functions/ip-tracker.js
@@ -1,7 +1,62 @@
+const getIPv4FromIPv6 = (ipv6) => {
+  if (!ipv6 || typeof ipv6 !== "string") {
+    return "";
+  }
+
+  const ipv4MappedPrefix = "::ffff:";
+  const ipv4Regex =
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+  if (ipv6.startsWith(ipv4MappedPrefix)) {
+    const ipv4Part = ipv6.substring(ipv4MappedPrefix.length);
+
+    if (ipv4Regex.test(ipv4Part)) {
+      return ipv4Part;
+    }
+  } else {
+    if (ipv4Regex.test(ipv6)) {
+      return ipv6;
+    }
+  }
+
+  return "";
+};
+
+const isPrivateIP = (ip) => {
+  if (!ip || typeof ip !== "string") {
+    return false;
+  }
+
+  var parts = ip.split(".");
+  return (
+    parts[0] === "10" ||
+    (parts[0] === "172" &&
+      parseInt(parts[1], 10) >= 16 &&
+      parseInt(parts[1], 10) <= 31) ||
+    (parts[0] === "192" && parts[1] === "168")
+  );
+};
+
+const getClientIP = (request) => {
+  const ipWithoutParsing = request.headers
+    .get("x-forwarded-for")
+    ?.split(/\s*,\s*/)[0];
+
+  const ip = getIPv4FromIPv6(ipWithoutParsing);
+
+  if (isPrivateIP(ip)) {
+    return "";
+  }
+
+  return ip;
+};
+
 export default async (request) => {
   const url = new URL(request.url);
 
-  const ip = url.searchParams.get("ip") ?? "";
+  const ip = url.searchParams.get("ip") || getClientIP(request);
+
+  console.log("Request IP: " + ip);
 
   const response = await fetch(
     `http://ip-api.com/json/${ip}?fields=status,message,country,regionName,city,lat,lon,timezone,isp,as,query`,
